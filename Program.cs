875d5d1c930b17058a7628b5abb160f083fd1ef5
Johannes Segerlund <js224em@student.lnu.se>
2020-03-30 22:19:27 +0200

    calculating max and min

--- Program.cs
+++ Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
-
+using Newtonsoft.Json;
+using System.IO;
+using System.Linq;
 namespace examination_1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] numberArray;
+            using (var reader = File.OpenText("data.json"))
+            {
+                var serilazier = new JsonSerializer();
+                numberArray = (int[])serilazier.Deserialize(reader, typeof(int[]));
+            }
+   
+            Max(numberArray);
+            Min(numberArray);
+        }
+
+        static void Max(int[] numberArray )
+        {
+            Console.WriteLine(numberArray.Max());
+        }
+        static void Min(int[] numberArray ) 
+        {
+            Console.WriteLine(numberArray.Min());
         }
     }
 }
